1338d9941e94bcf6922fcdac914fe8353c5926b3
Stefan Hagen <stefan@dilettant.eu>
2022-03-07 14:26:31 +0100

    Create middleware.go

---
+++ cmd/web/middleware.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+		next.ServeHTTP(w, r)
+	})
+}
