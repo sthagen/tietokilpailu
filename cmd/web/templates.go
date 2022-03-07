75661a3b5df7dd66c744e04459c69caf5cb18510
Stefan Hagen <stefan@dilettant.eu>
2022-03-07 14:27:52 +0100

    Create templates.go

---
+++ cmd/web/templates.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+	"time"
+)
+
+type templateData struct {
+	CurrentYear int
+	Quiz        RenderQuiz
+	Evaluation  RenderEvaluation
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		// The template.FuncMap must be registered with the template set before
+		// calling the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
+}
